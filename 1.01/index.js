76c5fe15329ba1f0c4f01ae8fa70b42eba138d5b
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-06-19 10:08:59 +0100

    curl the container to get middleware log

--- 1.01/index.js
+++ 1.01/index.js
@@ -3,6 +3,21 @@ const app = new Koa();
 
 const PORT = process.env.PORT || 3000;
 
+/*
+There might be another way to do this, but I had to
+
+1. Start by finding the pod running your application:
+   kubectl get pods -l app=<app-name>
+
+2. Once you have the pod name, execute a shell session inside the pod:
+   kubectl exec -it $(kubectl get pod -l app=<app-name> -o jsonpath="{.items[0].metadata.name}") -- /bin/sh
+
+3. Inside the pod's shell, use curl to access your application:
+   curl http://localhost:3000
+
+   Then I saw the log coming out of the middleware.
+*/
+
 app.use(async (ctx) => {
   ctx.body = `Server started in port ${PORT}`;
 });
