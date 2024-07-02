985268a3da380b5ac5a48e66678fc9bcb8f1ab21
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-07-02 21:35:33 +0100

    change backend end point

--- 2.02/todo-backend/index.js
+++ 2.02/todo-backend/index.js
@@ -6,11 +6,11 @@ app.use(express.json());
 
 let todos = ["Learn Node.js", "Learn Express.js", "Learn MongoDB"];
 
-app.get("/todos", (req, res) => {
+app.get("/", (req, res) => {
   res.json(todos);
 });
 
-app.post("/todos", (req, res) => {
+app.post("/", (req, res) => {
   const todo = req.body.todo;
   if (todo && todo.length > 0 && todo.length <= 140) {
     todos.push(todo);
